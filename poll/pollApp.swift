eb95dec81cb6df5d027565069302f74648542665
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-03-20 21:46:54 +0900

    Initial Commit

---
+++ poll/pollApp.swift
@@ -0,0 +1,17 @@
+//
+//  pollApp.swift
+//  poll
+//
+//  Created by lil-shimon on 2022/03/20.
+//
+
+import SwiftUI
+
+@main
+struct pollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
